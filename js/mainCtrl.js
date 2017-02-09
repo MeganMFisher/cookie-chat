3038aa5ba09b22756dc3a475db71a13d890fc5dc
MeganMFisher <meganmichelle318@gmail.com>
2017-02-08 17:16:02 -0700

    completed

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -3,20 +3,46 @@ angular.module('chatroom').controller('mainCtrl', function($scope, messageServic
   //The getMessages function will call the getMessages method on the messageService object.
   //You'll then save the result of that request to your controller's $scope as messages ($scope.messages)
 
+  $scope.getMessage = function(){
+      messageService.getMessage().then(function(result) {
+        // $scope.message = "";
+        $scope.messages = result.reverse();
+      });
+  }
+  $scope.getMessage();
 
+  $scope.postMessage = function(newMessage){
+    messageService.postMessage(newMessage).then(function(result) {
+
+      });
+  }
+
+   setInterval(function(){
+    $scope.getMessage();
+
+  }, 1500)
 
 
   //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
   //pass that text to the postMessage method on the messageService object which will
   //then post it to the backend.
 
-
-
   //uncomment this code when your getMessages function is finished
   //This goes and gets new data every second, which mimicking a chat room experience.
-  //  setInterval(function(){
-    // $scope.getMessages();
 
-  // }, 1500)
+
+$scope.getCookie = function() {
+    messageService.getCookie().then(function(result){
+      $scope.cookies = result;
+    });
+}
+
+$scope.getCookie();
+
+$scope.postCookie = function(newCookie){
+  messageService.postCookie(newCookie).then(function(result){
+
+  });
+}
 
 })
--- js/messageService.js
+++ js/messageService.js
@@ -5,15 +5,51 @@ angular.module('chatroom').service('messageService', function($http){
   //The url for the get request should be 'https://practiceapi.devmounta.in/api/chats'
   //Be sure to return whatever gets returned from $http so you can call .then in your controller.
 
+  this.getMessage = function() {
+    return $http({
+      method: 'GET',
+      url: 'https://practiceapi.devmounta.in/api/chats'
+
+    }).then(function(results){
+      
+        return results.data;
+    })
+  }
+ 
+
+  this.postMessage = function(yourMessage) {
+    return $http({
+      method: 'POST',
+      url: 'https://practiceapi.devmounta.in/api/chats',
+      data: {message: yourMessage}
+    })
+  }
 
 
 
   //On the line below create the postMessage method. This method will add data to the backend server.
   //The url for the request needs to be 'https://practiceapi.devmounta.in/api/chats'
-  //Because we're making a POST request, we need a way to tell the server the data we want to give it, in your $http call (along with url and method) have a data property which has a value that is equal to another object with a key of message and a value of the message being passed to parse. IE data: {message: yourMessage}
+  //Because we're making a POST request, we need a way to tell the server the data we want to give it, in your $http call (along with url and method)
+  // have a data property which has a value that is equal to another object with a key of message and a value of the message being passed to parse. IE data: {message: yourMessage}
   //Also, remember that $http returns a promise. So if you return the whole $http call (return $http(...)), you can then use .then in your controller.
 
 
 
+this.getCookie = function() {
+  return $http({
+    method: 'GET',
+    url: 'https://practiceapi.devmounta.in/api/cookies'
+  }).then(function(results){
+    return results.data;
+  })
+}
+
+this.postCookie = function(yourCookie) {
+  return $http({
+    method: 'POST',
+    url: 'https://practiceapi.devmounta.in/api/chats',
+    data: {cookie: yourCookie}
+  })
+}
 
 });
